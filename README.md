bd38871462a57dee2c60c96ec405753fcc594a05
archit-rohal <rohalarchit@gmail.com>
2023-03-28 11:29:17 +0530

    edit README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Random user generator 
 
-This app was built while learning about fetching data from APIs and manipulating the DOM using the data fetched.
+This app was built while learning about fetching data from public APIs using the fetch API and promises and manipulating the DOM using the data fetched.
+
+Public API used - https://randomuser.me/
 
 index.html and spinner.css were used as templates
--- script.js
+++ script.js
@@ -1,70 +1,35 @@
 function fetchUser() {
+  showSpinner();
   fetch('https://randomuser.me/api/').then(res => res.json()).then(data => {
+    hideSpinner();
     displayUser(data.results[0]);
   });
 }
 function displayUser(user) {
-  const userDisplay = document.querySelector('#user');
   if (user.gender === 'male') {
-    document.body.style.color = 'blue';
+    document.body.style.backgroundColor = 'blue';
   } else {
-    document.body.style.color = 'purple';
+    document.body.style.backgroundColor = 'purple';
   }
-  userDisplay.innerHTML = `
-  
-         <div class="flex justify-between">
-          <div class="flex">
-            <img
-              class="w-48 h-48 rounded-full mr-8"
-              src="https://randomuser.me/api/portraits/women/45.jpg"
-            />
-            <div class="space-y-3">
-              <p class="text-xl">
-                <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Email: </span> sarawilson@gmail.com
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Phone: </span> (555) 555-5555
-              </p>
-              <p class="text-xl">
-                <span class="font-bold">Location: </span> Boston MA
-              </p>
-              <p class="text-xl"><span class="font-bold">Age: </span> 30</p>
-            </div>
-          </div>
-        </div>
-  `
+  const userImage = document.querySelector('img');
+  userImage.setAttribute("src", `${user.picture.medium}`)
+  const userName = document.querySelector(".space-y-3").firstElementChild;
+  const userEmail = document.querySelector("p.text-xl:nth-child(2)");
+  const userPhone = document.querySelector("p.text-xl:nth-child(3)");
+  const userLocation = document.querySelector("p.text-xl:nth-child(4)");
+  const userAge = document.querySelector(".space-y-3").lastElementChild;
+  userName.innerHTML = `<span class="font-bold">Name:</span>${user.name.first} ${user.name.last}`;
+  userEmail.innerHTML = `<span class="font-bold">Email: </span>${user.email}`
+  userPhone.innerHTML = `<span class="font-bold">Phone: </span>${user.phone}`
+  userLocation.innerHTML = `<span class="font-bold">Location: </span>${user.location.street.name}`
+  userAge.innerHTML = `<p class="text-xl"><span class="font-bold">Age: </span>${user.dob.age}</p>`
 }
-// document.querySelector('#generate').textContent = data.results[0].gender;
-// function fetchData() {
-
-// fetch('https://randomuser.me/api/').then(res => res.json()).then(data =>
-//   document.querySelector('#user').innerHTML =
-//   `<div class="flex justify-between">
-//            <div class="flex">
-//              <img
-//                class="w-48 h-48 rounded-full mr-8"
-//                src= ${data.results[0].picture.medium}
-//              />
-//              <div class="space-y-3">
-//                <p class="text-xl">
-//                  <span class="font-bold">Name: </span>${data.results[0].name.first} ${data.results[0].name.last}
-//                </p>
-//                <p class="text-xl">
-//                  <span class="font-bold">Email: </span>${data.results[0].email}
-//                </p>
-//                <p class="text-xl">
-//                  <span class="font-bold">Phone: </span> (555) 555-5555
-//                </p>
-//                <p class="text-xl">
-//                  <span class="font-bold">Location: </span> Boston MA
-//                </p>
-//                <p class="text-xl"><span class="font-bold">Age: </span> 30</p>
-//              </div>
-//            </div>
-//          </div>
-//       </div>`);
-document.querySelector('#user').addEventListener('click', fetchUser);
+function showSpinner() {
+  document.querySelector('.spinner').style.display = 'block';
+}
+function hideSpinner() {
+  document.querySelector('.spinner').style.display = 'none';
+}
+document.querySelector('#generate').addEventListener('click', fetchUser);
 fetchUser();
+
